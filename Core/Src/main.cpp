f0368f1536c3728d7d40931eb100fee0ead71b8b
dj140 <1613270283@qq.com>
2023-10-09 02:47:23 +0800

    Finall

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -26,9 +26,13 @@
 #include "Wire.h"
 
 
-#define RESET_Pin PC6
-#define TP_INT_Pin PB5
-#define TE_IN_Pin PA8
+//#define RESET_Pin PC6
+//#define INT_Output_Pin PB5
+//#define INTn_ZT7548 PA8
+
+#define RESET_Pin PA15
+#define INT_Output_Pin PB5
+#define INTn_ZT7548 PB4
 
 //#define Soft_i2C
 #define HW_i2C
@@ -86,54 +90,54 @@ void MX_GPIO_Init(void)
 {
 
   LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+//  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
-  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOC);
+//  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
+//  LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOC);
   LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOB);
 
   /**/
-  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_6);
+//  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_6);
+
+//  /**/
+//  LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_5);
 
   /**/
-  LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_5);
+  LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTB, LL_EXTI_CONFIG_LINE4);
 
   /**/
-  LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE8);
-
-  /**/
-  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_8;
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_4;
   EXTI_InitStruct.LineCommand = ENABLE;
   EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
   EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_FALLING;
   LL_EXTI_Init(&EXTI_InitStruct);
 
   /**/
-  LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_8, LL_GPIO_PULL_UP);
+  LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_4, LL_GPIO_PULL_UP);
 
   /**/
-  LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_8, LL_GPIO_MODE_INPUT);
+  LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_INPUT);
 	
   /* EXTI interrupt init*/
   NVIC_SetPriority(EXTI4_15_IRQn, 1);
   NVIC_EnableIRQ(EXTI4_15_IRQn);
 
-  /**/
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+//  /**/
+//  GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
+//  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+//  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+//  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+//  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+//  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /**/
-  GPIO_InitStruct.Pin = LL_GPIO_PIN_5;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+//  /**/
+//  GPIO_InitStruct.Pin = LL_GPIO_PIN_5;
+//  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+//  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+//  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+//  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+//  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -168,7 +172,7 @@ void ZT7548_INT()
 	ZT7548.endTransmission(); 
 
 	#endif		
-	digitalWrite(TP_INT_Pin, LOW);
+	digitalWrite(INT_Output_Pin, LOW);
 //        delay(1);
 
 }
@@ -182,9 +186,9 @@ void EXTI4_15_IRQHandler(void)
   /* USER CODE BEGIN EXTI4_15_IRQn 0 */
 
   /* USER CODE END EXTI4_15_IRQn 0 */
-  if (LL_EXTI_IsActiveFallingFlag_0_31(LL_EXTI_LINE_8) != RESET)
+  if (LL_EXTI_IsActiveFallingFlag_0_31(LL_EXTI_LINE_4) != RESET)
   {
-    LL_EXTI_ClearFallingFlag_0_31(LL_EXTI_LINE_8);
+    LL_EXTI_ClearFallingFlag_0_31(LL_EXTI_LINE_4);
     /* USER CODE BEGIN LL_EXTI_LINE_15_RISING */
     
     /* Handle user button press in dedicated function */
@@ -237,11 +241,11 @@ int main(void)
   MX_IWDG_Init();
   /* USER CODE BEGIN 2 */
 	pinMode(RESET_Pin, OUTPUT);
-	pinMode(TP_INT_Pin, OUTPUT);
-	digitalWrite(TP_INT_Pin, HIGH);
+	pinMode(INT_Output_Pin, OUTPUT);
+	digitalWrite(INT_Output_Pin, HIGH);
 
-//    pinMode(TE_IN_Pin, INPUT_PULLUP);
-//    attachInterrupt(TE_IN_Pin, LED_Toogle, FALLING);
+//    pinMode(INTn_ZT7548, INPUT_PULLUP);
+//    attachInterrupt(INTn_ZT7548, LED_Toogle, FALLING);
 
 	digitalWrite(RESET_Pin, HIGH);
 	delay(10);
@@ -546,7 +550,7 @@ void Slave_Complete_Callback(void)
 {
 	if(Buffer_Rx_IIC1[0] == 0x10)
 	{
-			digitalWrite(TP_INT_Pin, HIGH);
+			digitalWrite(INT_Output_Pin, HIGH);
 	}
 }
 
